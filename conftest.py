677e9c7dc5a703aae350fb8792299c3bb279bc69
Artur Gizatullin <jhonconstantine1996@gmail.com>
2026-03-22 15:57:35 +0500

    change selenoid link

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ def fill_registration_form():
     options.add_argument("--incognito")
     options.add_argument("--start-maximized")
     driver = webdriver.Remote(
-       command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+       command_executor="https://user1:1234@ru.selenoid.autotests.cloud/wd/hub",
        options=options
     )
 
