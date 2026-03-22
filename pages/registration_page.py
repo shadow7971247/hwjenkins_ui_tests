29f1a03f2f17cd2380c74ed6beab4c13d4abae25
Artur Gizatullin <jhonconstantine1996@gmail.com>
2026-03-22 17:02:58 +0500

    delete selene import from pageobject

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from selene import browser
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
